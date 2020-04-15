b62e25b6189d790ac9a8658bc2fae6bda67d3d95
Shrey Patel <shrey_patel3@yahoo.com>
2020-04-15 14:33:04 -0400

    add/remove

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 /* eslint-disable */
 
-import React from 'react'
+import React, { useState } from 'react'
+import Todos from './Todos'
+import AddTodo from './AddTodo'
 import { hot } from 'react-hot-loader'
-import Todo from './Todo'
-import TodoList from './TodoList'
-import TodoApp from './TodoApp'
 
 class App extends React.Component {
+  state = {
+    todos: [
+      { id: 1, content: 'buy milk' },
+      { id: 2, content: 'play mario kart' }
+    ]
+  }
+
+  deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    })
+
+    this.setState({ todos })
+  }
+
+  addTodo = todo => {
+    todo.id = Math.random()
+
+    let todos = [...this.state.todos, todo]
+
+    this.setState({ todos })
+  }
+
   render() {
     return (
-      <div className="header">
-        <TodoApp />
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo App</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     )
   }
